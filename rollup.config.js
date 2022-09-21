5e5419aaf1e4f8b10c5e2e6ecee09e5c9ba48443
Vitaly <mirenrich@yandex.ru>
2022-09-21 19:51:57 +0300

    initial

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,19 @@
 import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+import pkg from './package.json';
 
 export default {
   input: 'src/index.ts',
-  output: {
-    dir: 'dist',
-    format: 'cjs',
-  },
-  plugins: [typescript()],
+  output: [
+    {
+      file: pkg.module,
+      format: 'es',
+    },
+    {
+      name: 'Cacher',
+      file: pkg.browser,
+      format: 'umd',
+    },
+  ],
+  plugins: [typescript(), terser()],
 };
